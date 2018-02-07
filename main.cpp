22241fca53af77b92a3936e36c4a89c2bd80f7ae
root <root@srv-vir-docker.indclocal.ch>
2018-02-07 16:32:55 +0100

    Mise à jour de test

--- main.cpp
+++ main.cpp
@@ -10,13 +10,14 @@ int main()
     cout << "Le premier nombre : ";
     cin >> firstNumber;
     cout << "Le second nombre : ";
+    cout << "----------------------------------" << endl;
     cin >> secondNumber;
 
     // sum of two numbers in stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
 
     // Prints sum 
-    cout << "Le résultat de : " << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers << endl;
+    cout << "Le résultat de : " << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers << endl <<endl;
     return 0;
   
 }
