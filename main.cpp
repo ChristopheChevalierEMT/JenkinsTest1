7793a19cb36a5d04c3b2d247a214a3d7e5e891df
root <root@srv-vir-docker.indclocal.ch>
2018-02-07 15:17:38 +0100

    Modification

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     int firstNumber, secondNumber, sumOfTwoNumbers;
     
+    cout << "Calcul de la somme de deux nombres" << endl;
+    cout << "----------------------------------" << endl;
     cout << "Le premier nombre : ";
     cin >> firstNumber;
     cout << "Le second nombre : ";
